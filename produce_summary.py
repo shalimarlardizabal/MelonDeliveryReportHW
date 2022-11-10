0002bf30352ee6bd2cfcc70e013a226e34212377
Shalimar Lardizabal <sdlardizabal@gmail.com>
2022-11-09 16:27:59 -0800

    Melon Homework Done

---
+++ produce_summary.py
@@ -0,0 +1,61 @@
+# print("Day 1")
+# the_file = open("um-deliveries-day-1.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[3]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+def print_melon_report(day, report):
+    print(f"Day {day}")
+    
+    the_file = open(report)
+    
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print (f"Delivered {count} {melon}s for total of ${amount}")
+    
+    the_file.close()
+
+print_melon_report(1, "um-deliveries-day-1.txt")
+print_melon_report(2, "um-deliveries-day-2.txt" )
+print_melon_report(3, "um-deliveries-day-3.txt")
